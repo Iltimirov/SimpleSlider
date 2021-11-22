4450e53f5c72571898bdc5b7cdbc18f2b2367fea
viktoriltimirov <iltimirovvictor@gmail.com>
2021-11-22 16:17:28 +0400

    added reanimated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import {useSharedValue} from 'react-native-reanimated'
 import {Slider} from './modules'
 
 export const App: React.FC = () => {
-  // const value = useSharedValue(0)
-  return <Slider min={0} max={100} step={10} />
+  const value = useSharedValue(0)
+  return <Slider min={0} max={100} step={10} value={value} />
 }
--- src/modules/uikit/slider/Slider.tsx
+++ src/modules/uikit/slider/Slider.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Text} from 'react-native'
+import Animated from 'react-native-reanimated'
 
 type Props = {
   min: number
   max: number
   step: number
+  value: Animated.SharedValue<number>
 }
 
 export const Slider: React.FC<Props> = () => {
