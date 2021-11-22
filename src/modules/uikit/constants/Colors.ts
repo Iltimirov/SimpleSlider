8e9bfcb3ca8c582b6e4eacb4e5efd9e02fa10f33
viktoriltimirov <iltimirovvictor@gmail.com>
2021-11-22 18:54:42 +0400

    remove extra

--- src/modules/uikit/constants/Colors.ts
+++ src/modules/uikit/constants/Colors.ts
@@ -1,11 +1,6 @@
 export const Colors = {
-  TRANSPARENT: '#0000',
   WHITE: '#FFF',
-  WHITE_40: '#FFF4',
-  GREEN: 'green',
-  GREY: '#96959B',
   GREY2: '#F1F3F5',
   PRIMARY: '#A6D9AF',
   PRIMARY_LIGHT: '#25B53F',
-  TOOLTIP_BLACK: '#1C1C1C',
 }
